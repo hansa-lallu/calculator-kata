04b926d4bac30bea48c9a0bad5d8d453e38e86d0
hansa-lallu <hansalallu@gmail.com>
2021-03-26 20:59:02 +1300

    test multiply operator within the string

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,9 +3,14 @@
 require 'calculator'
 
 describe Calculator do
-  it 'adds numbers within a string' do
+  it 'adds numbers when given an addition operator within the string' do
     calculator = Calculator.new
     expect(calculator.evaluate('4 + 5')).to eq(9)
     expect(calculator.evaluate('5 + 8')).to eq(13)
+  end
+
+  it 'multiplies number when given a multipy operator within the string' do
+    calculator = Calculator.new
+    expect(calculator.evaluate('4 * 5')).to eq(20)
   end
 end
