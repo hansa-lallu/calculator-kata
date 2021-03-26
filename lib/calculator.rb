547717653cc758642451c3939a8ff0271310b164
hansa-lallu <hansalallu@gmail.com>
2021-03-26 20:55:22 +1300

    can add a string of numbers

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Calculator
-  def evaluate(_string)
-    9
+  def evaluate(string)
+    array = string.split(' ').select.with_index { |_num, idx| idx.even? }.map(&:to_i)
+    array = array.inject(:+)
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,5 +6,6 @@ describe Calculator do
   it 'adds numbers within a string' do
     calculator = Calculator.new
     expect(calculator.evaluate('4 + 5')).to eq(9)
+    expect(calculator.evaluate('5 + 8')).to eq(13)
   end
 end
